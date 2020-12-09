01cb54901adb587bc279cf6c4e898679cbaeaf27
danielbuaron <75724440+danielbuaron@users.noreply.github.com>
2020-12-09 11:15:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_story(dani, x):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello {dani} from {country}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'This is a great day')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'I want to read {dani} {x} poems')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'an interesting story')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('')
+    print('')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_story('Daniel', 5)
+    print_story("michal", 7)
+    print_story("alon", 9)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
